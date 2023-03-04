41681b58a9843aadb36da2084ae7c6dc9d13ee70
Syl8n <magi8520@gmail.com>
2023-03-05 02:11:44 +0900

    refactor dbkey

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express'), app = express(), port = 5000
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+const { User } = require("./models/User")
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://dbadmin:1234@boiler-plate.fipwkvw.mongodb.net/?retryWrites=true&w=majority')
+const { mongoURI } = require('./config/dev')
+mongoose.connect(mongoURI)
 .then(() => console.log('MongoDB connected...'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World'))
+
+app.post('/signin', (req, res) => {
+    const user = new User(req.body)
+    user.save((err, doc) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({success: true})
+    })
+})
 
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`))
